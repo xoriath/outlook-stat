ae69ff24906e84897344f25384bc99be88d12ac6
xoriath <moro.engelhardt@gmail.com>
2018-08-15 08:03:51 -0700

    Inital version

---
+++ OutlookStat2/Controllers/StatController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OutlookTest;
+using Newtonsoft.Json;
+
+namespace OutlookStat2.Controllers
+{
+    [Route("api/unread")]
+    public class StatController : Controller
+    {
+        public class Datapoint {
+            public string x;
+            public int y;
+        }
+        public class Dataset {
+            public string label;
+            public IList<int> data = new List<int>();
+            public string backgroundColor;
+        }
+
+        public class Data {
+            public IList<string> labels = new List<string>();
+            public IList<Dataset> datasets = new List<Dataset>();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = new Data(); 
+
+            var unreadsByFolder = await Task.Run(() => OutlookAPI.FetchUnreads().ToList());
+            var colors = new Colours.ColourGenerator();
+
+            foreach (var unreadsInFolder in unreadsByFolder)
+            {
+                var dataset = new Dataset { label = unreadsInFolder.Key, backgroundColor =  "#" + colors.NextColour() };
+
+                var unreadInFolderByDate = unreadsInFolder.GroupBy(item => item.Received.Date);
+
+                foreach (var d in unreadInFolderByDate)
+                {
+                    //dataset.data.Add(new Datapoint { x = d.Key.Date.ToString("yyyy-MM-dd"), y = d.Count() });
+                    dataset.data.Add(d.Count());
+                }
+                data.datasets.Add(dataset);
+            }
+
+            var dates = unreadsByFolder.SelectMany(i => i.Select(t => t.Received.Date)).Distinct();
+            data.labels = dates.Select(d => d.ToString("yyyy-MM-dd")).ToList();
+            
+            return Ok(JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
+
+        public struct DataPoint {
+            public string Folder;
+            public int Count;
+        }
+    }
+}
---
+++ OutlookStat2/OutlookAPI.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OutlookTest
+{
+    class OutlookAPI
+    {
+        public struct Item 
+        {
+            private Outlook.Folder folder;
+            private Outlook.MailItem mail;
+            public DateTime Received => mail.ReceivedTime;
+            public String Subject => mail.Subject;
+            public String Folder => folder.Name;
+            
+
+            public Item(Outlook.Folder folder, Outlook.MailItem mail) 
+            {
+                this.folder = folder;
+                this.mail = mail;
+            }
+
+            public override string ToString() => $"[{Folder}] [{Received}] {Subject}";
+        }
+
+        public static IEnumerable<IGrouping<string, Item>> FetchUnreads()
+        {
+            var outlook = new Outlook.Application();
+            var mapi = outlook.GetNamespace("MAPI");
+            var inbox = (Outlook.Folder)mapi.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            var explorer = inbox.GetExplorer(false);
+
+            return EnumerateUnreads(inbox).OrderBy(i => i.Received).GroupBy(i => i.Folder);
+            
+        }
+
+        private static IEnumerable<Item> EnumerateUnreads(Outlook.Folder root)
+        {
+            foreach(Outlook.Folder folder in root.Folders)
+                foreach (Item item in EnumerateUnreads(folder))
+                    yield return item;
+
+            Outlook.Items items = root.Items.Restrict("[Unread] = true");
+            foreach (object item in items) 
+                if (item is Outlook.MailItem)
+                    yield return new Item(root, (Outlook.MailItem)item);
+        }
+    }
+}
